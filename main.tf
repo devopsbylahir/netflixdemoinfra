527d568a0174cbe94dd486c2507a814005447808
Lahir <devopsbylahir@gmail.com>
2025-05-21 00:51:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
 count = 4
 ami = "ami-06031e2c49c278c8f"
 instance_type = "t2.medium"
-key_name = "L.key"
+key_name = "L.Key"
 vpc_security_group_ids = ["sg-08783cc565c8cf9e1"]
 tags = {
 Name = var.instance_names[count.index]
