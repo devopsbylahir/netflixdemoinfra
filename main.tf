671b4a55aaae92919d15d3d0f112ddd0d78f17da
Lahir <devopsbylahir@gmail.com>
2025-05-20 14:28:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-region = "us-east-1"
+region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-04aa00acb1165b32a"
+ami = "ami-06031e2c49c278c8f"
 instance_type = "t2.medium"
-key_name = "argocd"
-vpc_security_group_ids = ["sg-06785bc60f40dceda"]
+key_name = "L.key"
+vpc_security_group_ids = ["sg-08783cc565c8cf9e1"]
 tags = {
 Name = var.instance_names[count.index]
 }
