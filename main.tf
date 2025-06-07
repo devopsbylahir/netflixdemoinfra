10dd4085415437d38982e0f971c1269f64ffcd05
Lahir <devopsbylahir@gmail.com>
2025-06-07 19:11:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ region = "ap-south-1"
 
 resource "aws_instance" "one" {
 count = 5
-ami = "ami-06031e2c49c278c8f"
+ami = "ami-09299e47e83cceaa7"
 instance_type = "t2.medium"
-key_name = "L.Key"
+key_name = "dev.ley"
 vpc_security_group_ids = ["sg-08783cc565c8cf9e1"]
 tags = {
 Name = var.instance_names[count.index]
